bf000cd202093cf57a84a62abf07b06fde4328cf
Patrick Templeton <contact@patricktempleton.xyz>
2016-05-02 16:28:49 -0400

    Add hp route

--- routes/headParse.js
+++ routes/headParse.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('header parser');
+router.get('/', function(req, res) {
+  res.render('headParse', {
+    title: "Request Header Parser",
+    shortDesc: "Get header info from HTTP request",
+    usage: "GET /hp/whoami",
+    output: '{ "ipaddress": your public ip address, "language": your language, "software": your operating system }'
+  });
+});
+
+router.get('/whoami', function(req, res) {
+  var info = {
+    "ipaddress": req.ip,
+    "language": req.headers["accept-language"].split(",")[0],
+    "software": req.headers["user-agent"].split("(")[1].split(")")[0]
+  };
+  res.json(info);
 });
 
 module.exports = router;
