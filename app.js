38e051177d7d376fee0df7a1363dd0ab754e5e91
Patrick Templeton <contact@patricktempleton.xyz>
2016-05-02 12:14:29 -0400

    Init

---
+++ app.js
@@ -0,0 +1,64 @@
+"use strict";
+
+let path       = require('path');
+let logger     = require('morgan');
+let express    = require('express');
+let bodyParser = require('body-parser');
+let favicon    = require('serve-favicon');
+
+let routes       = require('./routes/index');
+let headParse    = require('./routes/headParse');
+let imgSearch    = require('./routes/imgSearch');
+let timestamp    = require('./routes/timestamp');
+let urlShortener = require('./routes/urlShortener');
+let fileMetadata = require('./routes/fileMetadata');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/hp', headParse);
+app.use('/img', imgSearch);
+app.use('/ts', timestamp);
+app.use('/urls', urlShortener);
+app.use('/fmd', fileMetadata);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler - will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler - no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
